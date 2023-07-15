192e1a2c294374fa89af5f252a0f5a5ba4800f1f
Anshul Goyal <123931842+AnshulGoyal589@users.noreply.github.com>
2023-07-15 21:34:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
-const form = document.getElementById('locationForm');
-const locationInput = document.getElementById('locationInput');
-const weatherInfo = document.getElementById('weatherInfo');
-const locationElement = document.getElementById('location');
-const temperatureElement = document.getElementById('temperature');
-const humidityElement = document.getElementById('humidity');
-const windSpeedElement = document.getElementById('windSpeed');
+const xhr=new XMLHttpRequest();
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const location = locationInput.value;
-  getWeather(location);
-});
+let address=document.getElementById("location");
+let button=document.getElementById("submit-button");
+let ul=document.getElementById("wr");
+let block=document.getElementById("block");
+// let blockParent=document.getElementById("block-parent");
 
-async function getWeather(location) {
-  const apiKey = 'fd4aa4b2890e410f824173905230806'; 
-  const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`;
+button.addEventListener( "click",()=>{
 
-  try {
-    const response = await fetch(apiUrl);
-    if (!response.ok) {
-      throw new Error('Weather data not available');
+    xhr.open( 'get', `https://api.openweathermap.org/data/2.5/weather?q=${address.value}&appid=7e1dfced00e352855c083aa0b29b3add` );
+    xhr.send();
+} )
+
+let info1=document.createElement('div');
+let info2=document.createElement('div');
+let info3=document.createElement('div');
+let info4=document.createElement('div');
+let image1=document.createElement('img');
+let image2=document.createElement('img');
+let image3=document.createElement('img');
+let image4=document.createElement('img');
+let image5=document.createElement('img');
+const array=[image1 , image2 , image3 , image4 , image5];
+const array2=[ info1 , info2 , info3 ];
+for(let image of array){
+    image.className="blockImage";
+}
+for(let info of array2){
+    info.className="blockChild";
+}
+info4.className="blockChild2";
+
+xhr.onload=function(){
+    block.display="block";
+    let jsondata=this.response;
+    let weather=JSON.parse(jsondata);
+    let weatherIcon=weather.weather[0].icon;
+    console.log(weather);
+    for(let image of array){
+        image.src = `http://openweathermap.org/img/w/${weatherIcon}.png`;
     }
-    const data = await response.json();
+    info1.textContent="Temperature: "+(parseInt(weather.main.temp)-273).toString()+"°C";
+    info2.textContent="Humidity: "+weather.main.humidity;
+    info3.textContent="Wind Speed: "+weather.wind.speed;
+    info4.textContent=weather.name.toUpperCase();
+    for(let image of array){
+        block.appendChild(image);
+    }
+    block.appendChild(info4);
+    for(let info of array2){
+        block.appendChild(info);
+    }
 
-    locationElement.textContent = `Location: ${data.location.name}, ${data.location.country}`;
-    temperatureElement.textContent = `Temperature: ${data.current.temp_c}°C`;
-    humidityElement.textContent = `Humidity: ${data.current.humidity}%`;
-    windSpeedElement.textContent = `Wind Speed: ${data.current.wind_kph} km/h`;
-    weatherInfo.classList.remove('hidden');
-  } catch (error) {
-    alert('An error occurred. Please try again.');
-  }
+
+}
+xhr.onerror=function(){
+    
+    console.log("error aa raha hai");
+    
 }
